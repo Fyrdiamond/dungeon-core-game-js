868b72144603ba689a8cd11e312410198e39cbff
Leslie Obbard <fyrdiamond@gmail.com>
2026-02-28 10:36:17 -0500

    Move javascript code within window.onload event listener

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-const canvas = document.getElementById('canvas');
+window.addEventListener("load", () => {
+    const canvas = document.getElementById('canvas');
 
-var fullscreen = function () {
-    if (canvas.webkitRequestFullScreen) {
-        canvas.webkitRequestFullScreen();
-    } else {
-        canvas.mozRequestFullScreen();
+    function fullscreen() {
+        if (canvas.webkitRequestFullScreen) {
+            canvas.webkitRequestFullScreen();
+        } else {
+            canvas.mozRequestFullScreen();
+        }
     }
-}
 
-window.resize = function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+    function resize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
 
-window.addEventListener("resize", resize, false);
+    resize();
+    window.addEventListener("resize", resize, false);
+})
